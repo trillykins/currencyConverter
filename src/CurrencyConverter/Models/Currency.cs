41be7c0e280a98fe1f14ca31cc9e912348a33dc5
Kagemand Andersen <trillykins@gmail.com>
2022-03-08 18:10:32 +0100

    Test github action secret injection

---
+++ src/CurrencyConverter/Models/Currency.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter.Models
+{
+    public class Currency
+    {
+        [Required]
+        [Range(double.Epsilon, double.MaxValue, ErrorMessage = "The field {0} must be a positive, non-zero number.")]
+        public double Amount { get; set; }
+
+        [NotNull]
+        [Required]
+        public string CurrencyFrom { get; set; }
+
+        [NotNull]
+        [Required]
+        public string CurrencyTo { get; set; }
+    }
+}
